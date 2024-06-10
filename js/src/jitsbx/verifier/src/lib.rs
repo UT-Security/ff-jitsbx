448e8d134e4f062abc7014161370a68894e311fc
Abhishek Sharma <acsharma@cs.utexas.edu>
2024-06-10 11:58:57 -0500

    WIP

--- js/src/jitsbx/verifier/src/lib.rs
+++ js/src/jitsbx/verifier/src/lib.rs
@@ -7,6 +7,8 @@ use yaxpeax_x86::amd64::{InstDecoder, Instruction, Opcode, Operand, RegSpec};
 use yaxpeax_arch::{LengthedInstruction, AddressDiff, AddressBase};
 
 const BUNDLE_ALIGNMENT: usize = 32;
+const MAX_CODE_BYTES_PER_PROCESS: usize = 512 * 1024 * 1024;
+const INDIRECT_CODE_TARGET_MASM: i32 = ((MAX_CODE_BYTES_PER_PROCESS - 1) & !(BUNDLE_ALIGNMENT - 1)) as i32;  
 
 pub(crate) struct JitCodeInfo<'a> {
     code: &'a[u8],
@@ -102,7 +104,8 @@ impl<'a> Verifier<'a> {
                     | Operand::RegScaleDisp(_, _, _) 
                     | Operand::RegIndexBaseDisp(_, _, _) 
                     | Operand::RegIndexBase(_, _) 
-                    | Operand::RegScale(_, _) => false,
+                    | Operand::RegScale(_, _)
+                    | Operand::RegDisp(_, _) => false,
                     _ => true,
                 }
             },
@@ -124,8 +127,7 @@ impl<'a> Verifier<'a> {
             Opcode::CALL | Opcode::JMP => {
                 match instr.operand(0) {
                      Operand::Register(_)
-                     | Operand::RegDeref(_) 
-                     | Operand::RegDisp(_, _) => true,
+                     | Operand::RegDeref(_) => true,
                     _ => false,
                 }
             }
@@ -134,6 +136,94 @@ impl<'a> Verifier<'a> {
     }
 
     fn instr_is_jitsbx_jump_or_call(&self, instr: Instruction, ip: usize) -> bool {
-        true
+        let mut verifier = IndirectJumpVerifier::new(&self.info, ip, instr);       
+        
+        verifier.verify(&self.decoder)
     }
 } 
+
+struct IndirectJumpVerifier<'a> {
+    info: &'a JitCodeInfo<'a>,
+    ip: usize,
+    instr: Instruction,
+    is_call: bool,
+    register: RegSpec,
+    state: IndirectJumpVerifierState,
+}
+
+enum IndirectJumpVerifierState {
+    Initial,
+    Masked(usize),
+    Sandboxed(usize),
+}
+
+impl<'a> IndirectJumpVerifier<'a> {
+    pub fn new(info: &'a JitCodeInfo<'a>, ip: usize, instr: Instruction) -> Self {    
+        let is_call = match instr.opcode() {
+            Opcode::CALL => true,
+            Opcode::JMP => false,
+            _ => unreachable!(),
+        };
+
+        let register = match instr.operand(0) {
+            Operand::Register(register) => register,
+            Operand::RegDeref(register) => register,
+            _ => unreachable!(),
+        };
+
+        Self {
+            info,
+            ip,
+            instr,
+            is_call,
+            register,
+            state: IndirectJumpVerifierState::Initial,
+        }
+    }
+
+    pub fn verify(&mut self, decoder: &InstDecoder) -> bool {
+        // return address of call instruction should be bundle aligned.
+        if self.is_call && !((self.ip + self.instr.len().to_const() as usize) % BUNDLE_ALIGNMENT == 0) {
+            return false;
+        }
+
+        let current = (self.ip / BUNDLE_ALIGNMENT) * BUNDLE_ALIGNMENT;
+
+        while current < self.ip {
+            let instr = match decoder.decode_slice(&self.info.code[current..]) {
+                Ok(instr) => instr,
+                _ => unreachable!(),
+            };
+
+            match self.state {
+                IndirectJumpVerifierState::Initial => {
+                    current += instr.len().to_const() as usize;
+                    if instr.opcode() == Opcode::AND && self.is_jitsbx_mask(instr, current, decoder) {
+                        
+                    }
+                },
+                IndirectJumpVerifierState::Masked(mask_ip) => {
+                    
+                },
+                IndirectJumpVerifierState::Sandboxed(mask_ip) => {
+                    
+                }
+            }
+        }
+
+        true
+    }
+
+    fn is_jitsbx_mask(&self, instr: Instruction, ip: usize, decoder: &InstDecoder) -> bool {
+        /*match instr.operand(0) {
+            Operand::Register(RegSpec::d(self.register.num())) => (),
+            _ => return false,
+        }*/
+
+        /*match instr.operand(1) {
+            Operand::ImmediateI32(gn)
+        }*/
+
+        true
+    }
+}
