6c86aedfb8c2b7a42b005f50b835216d10f8920a
Abhishek Sharma <acsharma@cs.utexas.edu>
2025-05-30 19:15:19 -0500

    Use lfi-bind exposed allocation functions for JS Engine allocations

--- js/public/Utility.h
+++ js/public/Utility.h
@@ -378,10 +378,21 @@ extern void AssertJSStringBufferInCorrectArena(const void* ptr);
 
 } /* namespace js */
 
+#if defined(JS_SANDBOX_LFI) && defined(JS_SANDBOX_API)
+extern "C" void* monkeycage_moz_arena_malloc(arena_id_t, size_t);
+extern "C" void* monkeycage_moz_arena_calloc(arena_id_t, size_t, size_t);
+extern "C" void* monkeycage_moz_arena_realloc(arena_id_t, void*, size_t);
+extern "C" void monkeycage_free(void*);
+#endif
+
 static inline void* js_arena_malloc(arena_id_t arena, size_t bytes) {
   JS_OOM_POSSIBLY_FAIL();
   JS_CHECK_LARGE_ALLOC(bytes);
+#if defined(JS_SANDBOX_LFI) && defined(JS_SANDBOX_API)
+  return monkeycage_moz_arena_malloc(arena, bytes);
+#else
   return moz_arena_malloc(arena, bytes);
+#endif
 }
 
 static inline void* js_malloc(size_t bytes) {
@@ -395,14 +406,22 @@ static inline void* js_malloc(size_t bytes) {
 static inline void* js_arena_calloc(arena_id_t arena, size_t bytes) {
   JS_OOM_POSSIBLY_FAIL();
   JS_CHECK_LARGE_ALLOC(bytes);
+#if defined(JS_SANDBOX_LFI) && defined(JS_SANDBOX_API)
+  return monkeycage_moz_arena_calloc(arena, bytes, 1);
+#else
   return moz_arena_calloc(arena, bytes, 1);
+#endif
 }
 
 static inline void* js_arena_calloc(arena_id_t arena, size_t nmemb,
                                     size_t size) {
   JS_OOM_POSSIBLY_FAIL();
   JS_CHECK_LARGE_ALLOC(nmemb * size);
+#if defined(JS_SANDBOX_LFI) && defined(JS_SANDBOX_API)
+  return monkeycage_moz_arena_calloc(arena, nmemb, size);
+#else
   return moz_arena_calloc(arena, nmemb, size);
+#endif
 }
 
 static inline void* js_calloc(size_t bytes) {
@@ -429,7 +448,11 @@ static inline void* js_arena_realloc(arena_id_t arena, void* p, size_t bytes) {
 
   JS_OOM_POSSIBLY_FAIL();
   JS_CHECK_LARGE_ALLOC(bytes);
+#if defined(JS_SANDBOX_LFI) && defined(JS_SANDBOX_API)
+  return monkeycage_moz_arena_realloc(arena, p, bytes);
+#else
   return moz_arena_realloc(arena, p, bytes);
+#endif
 }
 
 static inline void* js_realloc(void* p, size_t bytes) {
@@ -445,7 +468,11 @@ static inline void js_free(void* p) {
   // currently can't enforce that all memory freed here was allocated by
   // js_malloc(). All other memory should go through a different allocator and
   // deallocator.
+#if defined(JS_SANDBOX_LFI) && defined(JS_SANDBOX_API)
+  return monkeycage_free(p);
+#else
   free(p);
+#endif
 }
 #endif /* JS_USE_CUSTOM_ALLOCATOR */
 
