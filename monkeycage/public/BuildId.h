91ad5cbf9a4e4866f59578e4f17cb94d763896eb
Abhishek Sharma <acsharma@cs.utexas.edu>
2025-05-30 19:14:43 -0500

    Register GetBuildId callbacks and expand MCAPI

---
+++ monkeycage/public/BuildId.h
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Embedding-provided build ID information, used by SpiderMonkey to tag cached
+ * compilation data so that cached data can be reused when possible, or
+ * discarded and regenerated if necessary.
+ */
+
+#ifndef mc_BuildId_h
+#define mc_BuildId_h
+
+#include "js/BuildId.h"
+
+#ifdef JS_SANDBOX
+
+#include "monkeycage/SandboxCallback.h"
+
+namespace JS {
+
+inline void SetProcessBuildIdOp(MC::SandboxCallback<BuildIdOp> buildIdOp) {
+  return SetProcessBuildIdOp(buildIdOp.UNSAFE_get());
+}
+
+}
+
+#endif
+
+#endif
--- monkeycage/public/ErrorReport.h
+++ monkeycage/public/ErrorReport.h
@@ -39,6 +39,24 @@ struct MOZ_STACK_CLASS JS_PUBLIC_API ErrorReportBuilder {
 
 }  // namespace MC
 
+template <typename... Args>
+inline void JS_ReportErrorASCII(MCContext* cx, const char* format,
+                                Args... args) {
+  return JS_ReportErrorASCII(cx->cx_, format, args...);
+}
+
+template <typename... Args>
+inline void JS_ReportErrorLatin1(MCContext* cx, const char* format,
+                                 Args... args) {
+  return JS_ReportErrorLatin1(cx->cx_, format, args...);
+}
+
+template <typename... Args>
+inline void JS_ReportErrorUTF8(MCContext* cx, const char* format,
+                               Args... args) {
+  return JS_ReportErrorUTF8(cx->cx_, format, args...);
+}
+
 inline MOZ_COLD void JS_ReportOutOfMemory(MCContext* cx) {
   return JS_ReportOutOfMemory(cx->cx_); 
 }
