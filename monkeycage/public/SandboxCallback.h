20537fed0bddd80c03673d9c15e175528d553d12
Abhishek Sharma <acsharma@cs.utexas.edu>
2025-06-09 19:26:40 -0500

    Use proper callbacks and separate memory allcator for Dylib sandbox

--- monkeycage/public/SandboxCallback.h
+++ monkeycage/public/SandboxCallback.h
@@ -47,21 +47,22 @@ template <typename T_Ret, typename... T_Args>
 class SandboxCallback<T_Ret(*)(T_Args...)> {
   using T_Cb = T_Ret (*)(T_Args...);
   
-  T_Cb fn_;
+  T_Cb app_fn_;
+  T_Cb sbx_fn_;
 
   friend class SandboxDylib;
-  explicit SandboxCallback(T_Cb fn) : fn_(fn) {}
+  explicit SandboxCallback(T_Cb app_fn, T_Cb sbx_fn) : app_fn_(app_fn), sbx_fn_(sbx_fn) {}
 
  public:
-  SandboxCallback(const std::nullptr_t& arg) : fn_(arg) {}
+  SandboxCallback(const std::nullptr_t& arg) : app_fn_(arg), sbx_fn_(arg) {}
 
-  T_Cb UNSAFE_get() const { return fn_; }
+  T_Cb UNSAFE_get() const { return sbx_fn_; }
 
-  operator bool() const { return fn_ == nullptr ? false : true; }
+  operator bool() const { return app_fn_ == nullptr ? false : true; }
 
   template <typename... Args>
   T_Ret operator()(Args&&... args) {
-    return fn_(std::forward<Args>(args)...);
+    return app_fn_(std::forward<Args>(args)...);
   }
 };
 #elif defined(JS_SANDBOX_LFI)
