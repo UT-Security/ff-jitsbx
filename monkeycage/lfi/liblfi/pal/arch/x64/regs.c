bcda5cbf8e123597c717a44428c8f625e76397af
Abhishek Sharma <acsharma@cs.utexas.edu>
2025-07-04 16:23:11 -0500

    Update liblfi 9dc7c77e420b8110b42611db645102ffce6050a3

--- monkeycage/lfi/liblfi/pal/arch/x64/regs.c
+++ monkeycage/lfi/liblfi/pal/arch/x64/regs.c
@@ -9,7 +9,6 @@ lfi_regs_init(struct TuxRegs* regs, struct LFIAddrSpace* as, struct LFIContext* 
     regs->rsp = as->base;
     regs->r15 = 0xffffffffffffffff;
     regs->r13 = (uintptr_t) ctx->sys;
-    regs->r15 = as->size - 1;
 }
 
 EXPORT void
