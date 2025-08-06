a39fa15125af8b623be428614e8bf7f0aeb6d8f9
Abhishek Sharma <acsharma@cs.utexas.edu>
2025-08-06 17:06:47 -0500

    Allocate JS::BuildIdCharVector on sandbox stack
    - Add initial TaintedVolatile template specialization for mozilla::Vector
    - Add Monkeycage APIs for fetching BuildId

---
+++ monkeycage/public/Vector.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mc_Vector_h
+#define mc_Vector_h
+
+#include "Tainted.h"
+#include "js/Vector.h"
+#include "monkeycage/Tainted.h"
+
+namespace MC {
+namespace detail {
+
+template <typename T, typename MC_Sbx, size_t MinInlineCapacity,
+          class AllocPolicy>
+class TaintedVolatile<js::Vector<T, MinInlineCapacity, AllocPolicy>, MC_Sbx> {
+private:
+  js::Vector<T, MinInlineCapacity, AllocPolicy> data;
+
+public:
+  Tainted<size_t, MC_Sbx> length() const {
+    return Tainted<size_t, MC_Sbx>(data.length());
+  }
+
+  Tainted<const T*, MC_Sbx> begin() const {
+    Tainted<const T*, MC_Sbx> ret(nullptr);
+    ret.assign_raw_pointer(data.begin());
+    return ret;
+  }
+};
+          
+}
+}
+
+#endif
