8a2684986e4b8bfb44f4a9e9aa599faa932bb094
Abhishek Sharma <acsharma@cs.utexas.edu>
2025-08-16 16:01:25 -0500

    Vendor latest liblfi (with patches)

--- monkeycage/lfi/liblfi/pal/platform.c
+++ monkeycage/lfi/liblfi/pal/platform.c
@@ -15,13 +15,13 @@ lfi_new_plat(struct LFIPlatOptions opts)
         return NULL;
 
     struct BoxMap* bm = boxmap_new((struct BoxMapOptions) {
-        .minalign = gb(4),
-        .maxalign = gb(4),
+        .minalign = gb(256),
+        .maxalign = gb(256),
         .guardsize = kb(80),
     });
     if (!bm)
         goto err1;
-    if (!boxmap_reserve(bm, gb(32)))
+    if (!boxmap_reserve(bm, gb(512)))
         goto err2;
 
     *plat = (struct LFIPlatform) {
