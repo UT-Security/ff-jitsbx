c83945445f6a82fcbdade9c28ceae0fa9b1f98c3
Abhishek Sharma <acsharma@cs.utexas.edu>
2025-11-16 13:09:39 -0600

    Update verifier

--- monkeycage/verifier/lfi-verify/test/x64_large/pass.s
+++ monkeycage/verifier/lfi-verify/test/x64_large/pass.s
@@ -160,3 +160,17 @@ divq 0x14(%rbp)
 ---
 // flags: --sandbox=stores
 btq %rax, (%rbx)
+---
+movq $0xb4, (%r14)
+---
+andq %r15, %r11
+movq (%r14, %r11), %rcx
+---
+andq %r15, %r11
+andq $-0x20, %r11
+addq %r14, %r11
+jmpq *%r11
+---
+andq $-0x10, %rsp
+andq %r15, %rsp
+addq %r14, %rsp
