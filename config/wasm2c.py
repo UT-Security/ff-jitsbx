22ae3cbcc815cb8bf6cc4d81d616b3864ea9e0ca
Shravan Narayan <shravanrn@gmail.com>
2026-06-18 15:57:53 -0500

    Allow configurable rlbox.lfi path

--- config/wasm2c.py
+++ config/wasm2c.py
@@ -4,14 +4,20 @@
 
 import os
 import subprocess
-import shutil
 
-def wasm2c(output, wasm2c_bin, wasm_lib, lfi_incstub):
+def wasm2c(output, wasm2c_bin, wasm_lib, lfi_incstub, *rlbox_lfibin_path):
     output.close()
     module_name = os.path.basename(os.path.splitext(wasm_lib)[0])
 
+    with open(lfi_incstub, 'r') as lfi_incstub_in:
+        content = lfi_incstub_in.read()
+
+    content = content.replace('REPLACE_ME_PATH', rlbox_lfibin_path[0])
+
     lfi_incstub_target = os.path.join(os.path.dirname(output.name), "lfiincstub.s")
-    shutil.copyfile(lfi_incstub, lfi_incstub_target)
+    with open(lfi_incstub_target, 'w') as lfi_incstub_out:
+        lfi_incstub_out.write(content)
+
 
     return subprocess.run(
         [wasm2c_bin, "-n", module_name, "-o", output.name, "--disable-simd", wasm_lib]
--- security/rlbox/lfiincstub.s
+++ security/rlbox/lfiincstub.s
@@ -2,7 +2,7 @@
 .global rlbox_lfi_start
 .global rlbox_lfi_end
 rlbox_lfi_start:
-    .incbin "rlbox.lfi"
+    .incbin "REPLACE_ME_PATH"
 rlbox_lfi_end:
 
 .section .note.GNU-stack,"",@progbits
